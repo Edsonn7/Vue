ff05d7cfd2a7c602cd8f8d393db1adbfebfe318d
Edsonn7 <139161078+Edsonn7@users.noreply.github.com>
2023-08-03 19:23:27 -0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Componentes</h1>
+  <hr>
+  <Card url="https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg" />
+  <Card/>
+  <Card/>
+</template>
+
+<script>
+import Card from '../componentes/Card.vue';
+export default {
+  components: {Card}
+}
+</script>
---
+++ Card.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <img src="url" alt="Vue.js">
+        <h2>Vue.js</h2>
+        <a href="https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg" target="blank">
+          Downland
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["url"],
+};
+</script>
+
+<style scoped>
+div{
+  display: inline-block;
+  text-align: center;
+  border: 1px solid #f2f2f2;
+  padding: 1%;
+  border-radius: 5px;
+  margin: 4px ;
+}
+
+img {
+  width: 10rem;
+  padding: 0.5rem;
+  background: antiquewhite;
+}
+
+a{
+    text-decoration: none;
+    color: snow;
+    background: #160c31;
+    padding: .5rem;
+    border-radius: 5px;
+    border: 1px solid #f2f2f2;
+}
+
+</style>
